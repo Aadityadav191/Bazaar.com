59c2566b8f7462743bedb8917f4fc6fce01bc297
Aadit Yadav <yaaditya191@gmail.com>
2025-07-14 11:36:50 +0545

    feat: add Cookies concent feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,35 @@ import { useState, useEffect } from "react";
 import Popup from "./Components/Popup";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import CookieConsent from './Components/CookieConsent';
 
 function App() {
   const [showPopup, setShowPopup] = useState(false);
+    const [showConsent, setShowConsent] = useState(false);
+
   useEffect(() => {
     setTimeout(() => {
       setShowPopup(true);
     }, 500);
+
+    const consent = localStorage.getItem('cookie_consent');
+    if (!consent) {
+      setTimeout(() => {
+        setShowConsent(true);
+      }, 5000);
+    }
   }, []);
+
+
+  const handleAccept = () => {
+    localStorage.setItem('cookie_consent', 'true');
+    setShowConsent(false);
+  };
 
   return (
     <>
       {showPopup && <Popup onClose={() => setShowPopup(false)} />}
+       {showConsent && <CookieConsent onAccept={handleAccept} />}
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
